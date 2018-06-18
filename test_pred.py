56ebee888504b34448e7382d63909e063af01822
SCIA Raspi <none@none.com>
2018-06-18 15:00:19 +0000

    test models

--- test_pred.py
+++ test_pred.py
@@ -11,7 +11,7 @@ INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
 #from ironcar import Ironcar
 
 #ironcar = Ironcar()
-file_path = "../resources/2018_06_13_16_44/frame_1585_gas_0.0_dir_0.0.jpg"
+file_path = "datasets/2018_06_13_16_44/frame_389_gas_0.0_dir_0.0.jpg"
 img = mpimg.imread(file_path)
 img = preprocess.preprocess(img)
 img = np.array([img])
