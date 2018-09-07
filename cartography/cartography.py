b360f6aa828f1a8f881b77392e082d8c1dddba22
Etienne <etienne.gandilon@epita.fr>
2018-09-07 16:56:54 +0200

    [Cartography] Shift from center of track managed

--- cartography/cartography.py
+++ cartography/cartography.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 
 class Cartography:
@@ -17,13 +18,24 @@ class Cartography:
     epsilon = 0.01
     # vectors
     displacements = []
+    circuit = []
 
     def __init__(self, road_width = 0.75, epsilon=0.01):
         self.road_width = road_width
         self.epsilon = epsilon
         self.path.append(self.cur_pos)
+        self.circuit.append(self.cur_pos)
 
-    def add_position(self, acceleration):
+    # angle in unit rad
+    def get_distance(self, angle):
+        shift = (angle / math.pi) + (math.pi / 2)
+        cos = math.cos(shift)
+        # value below varies depending on time taken to be on center
+        dist = np.linalg.norm(cos * self.cur_speed, ord=2)
+        print(dist)
+        return dist
+
+    def add_position(self, acceleration, angle):
         # has made a complete turn
         if len(self.path) > 20 and \
            np.linalg.norm(self.cur_pos, ord=2) < self.epsilon:
@@ -31,18 +43,34 @@ class Cartography:
 
         self.cur_speed = acceleration * self.dt + self.cur_speed
         self.cur_pos = self.cur_speed * self.dt + self.cur_pos
-        self.displacements.append(self.cur_pos - self.path[-1])
-        self.path.append(self.cur_pos)
+        displacement = self.cur_pos - self.path[-1]
+        self.displacements.append(displacement)
+
+        distance_from_center = np.zeros(2)
+        if np.any(displacement):
+            norm_v = np.asarray([ -displacement[1], displacement[0] ])
+            normal_vect = norm_v / np.linalg.norm(norm_v, ord=2)
+            distance_from_center = self.get_distance(angle) * normal_vect
+            if np.cross(normal_vect, displacement) < 0:
+                distance_from_center = -distance_from_center
+        self.path.append(self.cur_pos + distance_from_center)
+        self.circuit.append(self.cur_pos)
 
     def draw_circuit(self):
         self.path = np.asarray(self.path)
         self.displacements = np.asarray(self.displacements)
+        self.circuit = np.asarray(self.circuit)
         x = self.path[:, 0]
         y = self.path[:, 1]
 
         fig, ax = plt.subplots()
         ax.scatter(x, y, alpha=1, c='green')
+
+        x_p = self.circuit[:, 0]
+        y_p = self.circuit[:, 1]
+
         plt.plot(x, y)
+        plt.plot(x_p, y_p, 'grey', alpha = 0.6)
 
         """
         # computing left and right points
