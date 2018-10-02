1d95e2732f6a56dcf4d4f6983805b9418450c98f
Maxime <maxime2.blanc@epita.fr>
2018-10-02 16:26:00 +0200

    Test Prediction FPS

--- models/test_model_time.py
+++ models/test_model_time.py
@@ -5,8 +5,14 @@ import time
 import numpy as np
 import md
 
-mod = md.build_model()
-mod.load_weights("model-0,0YUV.h5")
+from keras import backend as K
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
+
+
+#mod = md.build_model()
+mod = load_model("model-test-lstm.hdf5", custom_objects={'root_mean_squared_error': root_mean_squared_error})
 it = 1000
 file_path = "test.jpg"
 start_time = time.time()
@@ -14,7 +20,7 @@ start_time = time.time()
 for i in range(it):
     img = cv2.imread(file_path)
     img, _ = preprocess.preprocess(img)
-    img = np.array([img])
+    img = np.array([[img]])
     mod.predict(img)
 
 print("--- %s fps ---" % str(it/(time.time() - start_time)))
