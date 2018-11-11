b59ad7ec54d8d81bba25e1a73865804e7672762f
Alae <alae-edine.el-hmimdi@epita.fr>
2018-11-11 18:31:32 +0100

    Test relu squeeze

--- main/squeeze_net/squeeze_model.py
+++ main/squeeze_net/squeeze_model.py
@@ -11,6 +11,7 @@ from nvi_utils import INPUT_SHAPE, batch_generator
 import argparse
 import os
 from keras import backend as K
+from keras.utils.vis_utils import plot_model
 
 np.random.seed(0)
 
@@ -34,11 +35,11 @@ def load_data(args):
 
 
 def fire(x, squeeze=16, expand=64):
-    x = Conv2D(squeeze, (1,1), activation='elu', padding='valid')(x)
+    x = Conv2D(squeeze, (1,1), activation='relu', padding='valid')(x)
     
-    left = Conv2D(expand, (1,1), activation='elu', padding='valid')(x)
+    left = Conv2D(expand, (1,1), activation='relu', padding='valid')(x)
     
-    right = Conv2D(expand, (3,3), activation='elu', padding='same')(x)
+    right = Conv2D(expand, (3,3), activation='relu', padding='same')(x)
     
     x = concatenate([left, right], axis=3)
     return x
@@ -49,7 +50,7 @@ def build_model(args):
     """
     img_input = Input(shape=INPUT_SHAPE)
 
-    x = Conv2D(64, (3, 3), activation='elu', strides=(2, 2), padding='valid')(img_input)
+    x = Conv2D(64, (3, 3), activation='relu', strides=(2, 2), padding='valid')(img_input)
     x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(x)
 
     x = fire(x, squeeze=16, expand=16)
@@ -66,13 +67,13 @@ def build_model(args):
     #x = fire(x, squeeze=64, expand=64)
     x = Dropout(args.keep_prob)(x)
 
-    x = Conv2D(5, (1, 1), activation='elu', padding='valid')(x)
+    x = Conv2D(5, (1, 1), activation='relu', padding='valid')(x)
     x = Flatten()(x)
 
     out = Dense(1, activation='linear')(x)
 
     model= Model(img_input, out)
-
+    plot_model(model, to_file='images/SqueezeNet.png', show_shapes=True)
     model.summary()
     return model
 
