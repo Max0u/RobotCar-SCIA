fb0ba7946a24a681f12e5d75e335bed9fe8b0bfa
Maxime <maxime2.blanc@epita.fr>
2018-11-17 17:14:32 +0100

    Find best fps setting

---
+++ best_fps.py
@@ -0,0 +1,47 @@
+import numpy as np
+from picamera import PiCamera
+import time
+import matplotlib.pyplot as plt
+from picamera.array import PiRGBArray
+
+from ironcar_light import Ironcar
+
+iron = Ironcar()
+iron.load_config()
+iron.select_model("models/model-sq48-112,48-ep90.h5")
+iron.switch_mode("auto")
+
+CAM_RESOLUTION = (200, 146)
+loop = 10
+true_fps = []
+camera = PiCamera()
+start = 20
+end = 90
+
+cam_output = PiRGBArray(camera, size=CAM_RESOLUTION)
+stream = camera.capture_continuous(cam_output, format="rgb", use_video_port=True)
+
+for fps in range(start, end):
+    print(fps)
+    camera.framerate = fps
+    camera.resolution = CAM_RESOLUTION
+    save_time = time.time()
+    count = 0
+    for f in stream:
+        img_arr = f.array
+        prediction = iron.predict_from_img(img_arr)
+        iron.mode_function(img_arr, prediction)
+        cam_output.truncate(0)
+        count += 1
+        if count == loop:
+            break
+    true_fps.append(loop/(time.time()-save_time))
+
+
+plt.plot(range(start, end), range(start, end), color='green', label='Theory')
+plt.plot(range(start, end), true_fps, label='Practice')
+plt.xlabel("FPS Set")
+plt.ylabel("FPS Get")
+plt.legend()
+
+plt.savefig('best_fps.png')
